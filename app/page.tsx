d67d81bbff9b1a33dfa498f192be335eb8bc2ab6
Lauren <lauren@therise-x.com>
2026-03-10 00:17:48 -0400

    fix: restore missing > on main tag to fix JSX syntax error

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 const APP_STORE_URL = "https://apps.apple.com/app/id6744145553"
 export default function Page() {
   return (
-    <main className="page"
+    <main className="page">
       <TopNav />
       <Hero />
       <PhoneShowcase />
